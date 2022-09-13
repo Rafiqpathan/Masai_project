a1e49e1324232726aadb0c0cad63e8230e854686
SumitUjjwal <sumitonboard@gmail.com>
2022-09-14 00:09:36 +0530

    fixed the code

--- Palindrome_Problem.js
+++ Palindrome_Problem.js
@@ -1,9 +1,9 @@
 function checkPalindrome(N, str) {
    var new_str="";
-    for(i=N-1; i>=0; i++){
+    for(i=N-1; i>=0; i--){
         new_str=new_str+str[i];
     }
-    if(new_str=str){
+    if(new_str==str){
         console.log("Yes");
     } else {
         console.log("No");
